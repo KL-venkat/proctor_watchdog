94705bb6ebfb440d5b4c19ed960c491fdc27901f
venkat <venkatkoppula137@gmail.com>
2024-08-25 22:17:37 +0530

    added deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/proctor_watchdog/"
+  base: "/proctor_watchdog/",
+  build: { chunkSizeWarningLimit: 1600,}
 })
